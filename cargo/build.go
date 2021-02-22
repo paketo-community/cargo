e4ec8d8b68719e56dcb001fb3d60419d6333c34e
Forest Eckhardt <feckhardt@pivotal.io>
2021-02-22 13:08:30 -0500

    Updates all of the git modules and makes tests pass (#6)
    - Uses built in clock and log emitter now.

--- cargo/build.go
+++ cargo/build.go
@@ -5,17 +5,18 @@ import (
 	"time"
 
 	"github.com/paketo-buildpacks/packit"
+	"github.com/paketo-buildpacks/packit/chronos"
 	"github.com/paketo-buildpacks/packit/scribe"
 )
 
-//go:generate mockery -name Summer -case=underscore
+//go:generate mockery --name Summer --case=underscore
 
 // Summer can make checksums of the item at the given path
 type Summer interface {
-	Sum(path string) (string, error)
+	Sum(path ...string) (string, error)
 }
 
-//go:generate mockery -name Runner -case=underscore
+//go:generate mockery --name Runner --case=underscore
 
 // Runner is something capable of running Cargo
 type Runner interface {
@@ -23,20 +24,25 @@ type Runner interface {
 }
 
 // Build does the actual install of Rust
-func Build(runner Runner, summer Summer, clock Clock, logger *scribe.Logger) packit.BuildFunc {
+func Build(runner Runner, summer Summer, clock chronos.Clock, logger scribe.Emitter) packit.BuildFunc {
 	return func(context packit.BuildContext) (packit.BuildResult, error) {
 		logger.Title("%s %s", context.BuildpackInfo.Name, context.BuildpackInfo.Version)
 		logger.Process("Cargo is checking if your Rust project needs to be built")
 
-		cargoLayer, err := context.Layers.Get("rust-cargo", packit.BuildLayer, packit.CacheLayer)
+		cargoLayer, err := context.Layers.Get("rust-cargo")
 		if err != nil {
 			return packit.BuildResult{}, err
 		}
 
-		binaryLayer, err := context.Layers.Get("rust-bin", packit.LaunchLayer)
+		cargoLayer.Build = true
+		cargoLayer.Cache = true
+
+		binaryLayer, err := context.Layers.Get("rust-bin")
 		if err != nil {
 			return packit.BuildResult{}, err
 		}
+
+		binaryLayer.Launch = true
 
 		cargoLockHash, err := summer.Sum(filepath.Join(context.WorkingDir, "Cargo.lock"))
 		if err != nil {
