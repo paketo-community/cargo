3da05b9e1036a71ee5717307467e338f8bbdc0d1
Daniel Mikusa <dmikusa@vmware.com>
2021-03-24 22:25:28 -0400

    Combine detect & build into one command, run.

--- run/main.go
+++ run/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	rustCargo "github.com/dmikusa/rust-cargo-cnb/cargo"
+	"github.com/dmikusa/rust-cargo-cnb/cargo"
 	"github.com/paketo-buildpacks/packit"
 	"github.com/paketo-buildpacks/packit/chronos"
 	"github.com/paketo-buildpacks/packit/fs"
@@ -16,5 +16,8 @@ func main() {
 	checksumCalculator := fs.NewChecksumCalculator()
 	logger := scribe.NewEmitter(os.Stdout)
 
-	packit.Build(rustCargo.Build(rustCargo.NewCLIRunner(cargoExe), checksumCalculator, chronos.DefaultClock, logger))
+	packit.Run(
+		cargo.Detect(),
+		cargo.Build(cargo.NewCLIRunner(cargoExe), checksumCalculator, chronos.DefaultClock, logger),
+	)
 }
